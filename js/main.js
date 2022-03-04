24ba5c1b48a8a378175a1780d5367bdf582c47ce
VsNik <v.joxow@gmail.com>
2022-03-04 12:36:39 +0300

    fixes generators

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 const getRandomInt = (min, max) => {
   if (max <= min) {
-    // eslint-disable-next-line no-console
-    console.log('Максимальное значение должно быть больше минимального.');
-    return false;
+    throw Error('Максимальное значение должно быть больше минимального.');
   }
 
   if (min < 0) {
-    return false;
+    return NaN;
   }
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -14,13 +12,11 @@ const getRandomInt = (min, max) => {
 
 const getRandom = (min, max, countSigns = 2) => {
   if (max <= min) {
-    // eslint-disable-next-line no-console
-    console.log('Максимальное значение должно быть больше минимального.');
-    return false;
+    throw Error('Максимальное значение должно быть больше минимального.');
   }
 
   if (min < 0 || countSigns < 0) {
-    return false;
+    return NaN;
   }
 
   const rand = Math.random() * (max - min + 1) + min;
