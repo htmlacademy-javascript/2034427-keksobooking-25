ec3431f1ac81ce26bc1f29fed6482c39f9ff5caa
VsNik <v.joxow@gmail.com>
2022-03-15 13:49:52 +0300

    module8-task2

--- js/form.js
+++ js/form.js
@@ -1,12 +1,23 @@
 const adForm = document.querySelector('.ad-form');
 const titleField = document.querySelector('#title');
+const typeField = document.querySelector('#type');
 const priceField = document.querySelector('#price');
 const roomsField = document.querySelector('#room_number');
 const capacityField = document.querySelector('#capacity');
+const timeInField = document.querySelector('#timein');
+const timeOutField = document.querySelector('#timeout');
 
 const MIN_LENGTH_TITLE = 30;
 const MAX_LENGTH_TITLE = 100;
 const MAX_PRICE = 1000000;
+
+const minPrices = {
+  'bungalow': 0,
+  'flat': 1000,
+  'hotel': 3000,
+  'house': 5000,
+  'palace': 10000,
+};
 
 const roomCapacities = {
   1: [1],
@@ -28,13 +39,33 @@ const pristine = new Pristine(adForm, {
   errorTextParent: 'ad-form__element',
   errorTextTag: 'div',
   errorTextClass: 'text-help'
-});
+}, true);
 
-pristine.addValidator(priceField, (valuePrice) => valuePrice >= parseInt(priceField.min, 10) && valuePrice <= MAX_PRICE,
-  `Цена за ноч от ${priceField.min} до ${MAX_PRICE}`);
+const setAdType = () => {
+  priceField.min = minPrices[typeField.value];
+  priceField.placeholder = minPrices[typeField.value];
+};
 
-pristine.addValidator(titleField, (symbols) => symbols.length >= MIN_LENGTH_TITLE && symbols.length <= MAX_LENGTH_TITLE ,
-  `Длинна объявления должна быть от ${MIN_LENGTH_TITLE} до ${MAX_LENGTH_TITLE} символов.`);
+const getPriceValidatorErrors = () => {
+  if (parseInt(priceField.value, 10) < parseInt(priceField.min, 10)) {
+    return `Минимальная цена ${priceField.min}`;
+  }
+  if (parseInt(priceField.value, 10) > MAX_PRICE) {
+    return `Максимальная цена ${MAX_PRICE}`;
+  }
+};
+
+pristine.addValidator(
+  priceField,
+  (valuePrice) => valuePrice >= parseInt(priceField.min, 10) && valuePrice <= MAX_PRICE,
+  getPriceValidatorErrors
+);
+
+pristine.addValidator(
+  titleField,
+  (symbols) => symbols.length >= MIN_LENGTH_TITLE && symbols.length <= MAX_LENGTH_TITLE ,
+  `Длинна объявления должна быть от ${MIN_LENGTH_TITLE} до ${MAX_LENGTH_TITLE} символов.`
+);
 
 const roomsCapacityValidator = () => {
   const countRooms = parseInt(roomsField.value, 10);
@@ -48,14 +79,25 @@ const getRoomsCapacityErrorMessage = () => {
   const textRooms = roomsField.options[roomsField.selectedIndex].textContent;
   if (countRooms === 100) {
     return `${textRooms} не для гостей`;
-  } else {
-    return `${textRooms} не более ${capacityNumberToText[maxCapacities]}`;
   }
+
+  return `${textRooms} не более ${capacityNumberToText[maxCapacities]}`;
 };
 
 pristine.addValidator(roomsField, roomsCapacityValidator, getRoomsCapacityErrorMessage);
 
 capacityField.addEventListener('change', () => pristine.validate(roomsField));
+
+timeInField.addEventListener('change', () => {timeOutField.value = timeInField.value;});
+
+timeOutField.addEventListener('change', () => {timeInField.value = timeOutField.value;});
+
+typeField.addEventListener('change', () => {
+  setAdType();
+  pristine.validate(priceField);
+});
+
+setAdType();
 
 adForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
